a58ed559a677662faede404bcd3fe93e9094b26f
Constace <93983712+constance012@users.noreply.github.com>
2022-08-09 19:15:58 +0700

    Delete Just A Cube directory

--- Just A Cube/Assets/Scripts/Credits.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Credits : MonoBehaviour
-{
-	public void BackToMenu()
-	{
-		FindObjectOfType<LoadNextLevel>().Load("Scenes/Menu");
-	}
-}
--- Just A Cube/Assets/Scripts/Pattern.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Pattern : MonoBehaviour
-{
-	public Transform[] spawnPoints;    
-}
